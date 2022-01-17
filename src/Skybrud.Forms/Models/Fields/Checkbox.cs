fb4cbf79ea2d2fa81157f0528e7f87777549198e
Anders Bjerner <abjerner@skybrud.dk>
2022-01-17 16:56:28 +0100

    Checkbox class should have a IsChecked property

--- src/Skybrud.Forms/Models/Fields/Checkbox.cs
+++ src/Skybrud.Forms/Models/Fields/Checkbox.cs
@@ -1,15 +1,26 @@
-﻿namespace Skybrud.Forms.Models.Fields {
+﻿using Newtonsoft.Json;
+
+namespace Skybrud.Forms.Models.Fields {
 
     /// <summary>
     /// Class representing a HTML <c>checkbox</c> field.
     /// </summary>
     public class Checkbox : InputField {
 
+        [JsonProperty("checked")]
+        public bool IsChecked { get; set; }
+
         public Checkbox() : base("checkbox") { }
 
         public Checkbox(string name, string value) : base("checkbox") {
             Name = name;
             Value = value;
+        }
+
+        public Checkbox(string name, string value, bool isChecked) : base("checkbox") {
+            Name = name;
+            Value = value;
+            IsChecked = isChecked;
         }
 
     }
