c15c9b029330e3c49b09f55f4741691ce18797fd
Anders Bjerner <abjerner@skybrud.dk>
2023-01-04 19:35:43 +0100

    Added unit test to confirm that #1 has been fixed

---
+++ src/TestProject1/DateFieldTests.cs
@@ -0,0 +1,19 @@
+﻿using Skybrud.Forms.Models.Fields;
+
+namespace TestProject1 {
+
+    [TestClass]
+    public class DateFieldTests {
+
+        [TestMethod]
+        public void ValidateType() {
+
+            var field = new DateField();
+
+            Assert.AreEqual("date", field.Type);
+
+        }
+
+    }
+
+}
